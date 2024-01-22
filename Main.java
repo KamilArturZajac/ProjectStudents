c8f43de443e2d317fc43ada6ad8f2cf5dffa8daf
KamilArturZajac <157361644+KamilArturZajac@users.noreply.github.com>
2024-01-22 12:56:35 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,17 @@
-package com.capgemini.programowanie.obiektowe;
+package pl.wsb.university;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        WarehouseConfigure warehouseConfigure = new WarehouseConfigure();
-        String metal1 = warehouseConfigure.addMetalIngot("4565", SupportedMetalType.COPPER, 10.01);
-
-        ClientConfigure clientConfigure = new ClientConfigure();
+        UniversityManager universityManager = new UniversityManager();
         //Tworzenie przykładowych klientów
-        String client1 = clientConfigure.createNewClient("Jan", "Kowalski");
-        String client2 = clientConfigure.createNewClient("Mateusz", "Nowak");
-        String client3 = clientConfigure.createNewClient("Janina", "Kowalczyk");
+        String student1 = universityManager.recruitNewStudent("Jan", "Kowalski", LocalDate.of(1991, 1, 1));
+        String student2 = universityManager.recruitNewStudent("Mateusz", "Nowak", LocalDate.of(1992, 2, 2));
+        String student3 = universityManager.recruitNewStudent("Janina", "Kowalczyk", LocalDate.of(1993, 3, 3));
 
-        //Aktywowanie premium dla klienta 1
-        clientConfigure.activatePremiumAccount(client1);
-
-        //Wywoływanie pełnego imienia i nazwiska klienta 1
-        String fullName = clientConfigure.getClientFullName(client1);
-        System.out.println(fullName);
-
-        //Wywoływanie daty utworzenia klienta 2
-        LocalDate creationDate = clientConfigure.getClientCreationDate(client2);
-        System.out.println(creationDate);
-
-        //Wywoływanie statusu premium klienta 1
-        boolean isPremium = clientConfigure.isPremiumClient(client1);
-        System.out.println(client1 + " is premium? " + isPremium);
-
-        //Wywoływanie liczby klientów
-        int numberOfClients = clientConfigure.getNumberOfClients();
-        System.out.println("Total number of clients: " + numberOfClients);
-
-        //Wywoływanie liczby klientów premium
-        int numberOfPremiumClients = clientConfigure.getNumberOfPremiumClients();
-        System.out.println("Total number of premium clients: " + numberOfPremiumClients);
+        //Wywoływanie pełnego imienia i nazwiska studenta 1
+        String studentFullName = universityManager.getStudentFullName(student1);
+        System.out.println(studentFullName);
     }
 }
